a388a5d7f4c799031256adfa6c3dab102faa9baa
Legonius <emoinblackzone@gmail.com>
2024-07-23 00:42:12 +0630

    get method for conversations between 2 users

--- Backend/controllers/msgController.js
+++ Backend/controllers/msgController.js
@@ -24,9 +24,12 @@ const msgSend = async (req, res, next) => {
       receiverId,
       message,
     });
-    conversation.messages.push(newMessage._id);
-
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
     await Promise.all([conversation.save(), newMessage.save()]);
+    //Socket io Functionallity session
+
     res.status(201).json({ success: true, newMessage });
 
     // session.commitTransaction();
@@ -38,4 +41,19 @@ const msgSend = async (req, res, next) => {
   }
 };
 
-export { msgSend };
+const getMsg = async (req, res, next) => {
+  const { id: otherUser } = req.params;
+  const userId = req.user._id;
+
+  const conversation = await conversationModel
+    .findOne({
+      participants: { $all: [otherUser, userId] },
+    })
+    .populate("messages");
+  if (!conversation) {
+    return res.status(201).json([]);
+  }
+  res.status(201).json(conversation.messages);
+};
+
+export { msgSend, getMsg };
--- Backend/routes/message.routes.js
+++ Backend/routes/message.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { msgSend } from "../controllers/msgController.js";
+import { getMsg, msgSend } from "../controllers/msgController.js";
 import { checkAuthen } from "../middlewares/checkAuthentication.js";
 
 const msgRoute = express.Router();
 msgRoute.use(checkAuthen);
 
 msgRoute.post("/send/:id", msgSend);
+msgRoute.get("/:id", getMsg);
 
 export default msgRoute;
