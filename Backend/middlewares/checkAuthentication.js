b27d4d1bff374122f1db34c2ec440d6ae369871a
Legonius <emoinblackzone@gmail.com>
2024-07-22 15:40:34 +0630

    handling error of middleware

--- Backend/middlewares/checkAuthentication.js
+++ Backend/middlewares/checkAuthentication.js
@@ -1,16 +1,27 @@
 import jwt from "jsonwebtoken";
 import { userModel } from "../models/userModel.js";
+import { errorHandling } from "../utils/error.js";
 
 export const checkAuthen = async (req, res, next) => {
-  const token = req.cookies.uid;
-  if (!token) {
-    return next();
+  try {
+    const token = req.cookies.uid;
+    if (!token) {
+      return next();
+    }
+    const payload = jwt.verify(token, process.env.JWT_CODE);
+    if (!payload) {
+      next(errorHandling(403, "User need to login"));
+    }
+    const user = await userModel.findOne({
+      username: payload.username,
+      email: payload.email,
+    });
+    if (!user) {
+      next(errorHandling(403, "User need to login"));
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
   }
-  const payload = jwt.verify(token, process.env.JWT_CODE);
-  const user = await userModel.findOne({
-    username: payload.username,
-    email: payload.email,
-  });
-  req.user = user;
-  next();
 };
