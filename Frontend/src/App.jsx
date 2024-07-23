8b5e442fd572579fbeca7a71c30d5d75f0e25fc5
Legonius <emoinblackzone@gmail.com>
2024-07-23 23:02:35 +0630

    frame ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,11 @@ function App() {
   return (
     <>
       <div className="flex justify-center items-center h-screen bg-slate-500">
-        <div className="artboard phone-1 bg-gray-300">320×568</div>
+        <div className="artboard phone-1 bg-gray-300 rounded-lg">
+          <div className="h-12 bg-slate-400 rounded-t-lg text-slate-50 font-extrabold text-xl flex items-center justify-start px-2">
+            Watt's Chatt
+          </div>
+        </div>
       </div>
     </>
   );
