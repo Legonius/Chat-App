7616a61d6755bea8062f5823b9855ddcbfec0b83
Legonius <emoinblackzone@gmail.com>
2024-11-22 17:20:43 +0630

    sesion expire func disable

--- Frontend/src/utils/sessionExpire.js
+++ Frontend/src/utils/sessionExpire.js
@@ -2,10 +2,10 @@ import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 const sessionExpire = () => {
-  const navigate = useNavigate();
-  localStorage.removeItem("chat-app-user");
-  navigate("/");
-  return toast.error("Session Expired");
+  // const navigate = useNavigate();
+  // localStorage.removeItem("chat-app-user");
+  // navigate("/");
+  // return toast.error("Session Expired");
 };
 
 export default sessionExpire;
