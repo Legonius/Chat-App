3587aac4661f655e82c4c8374e5aad7dd0156e9d
Legonius <emoinblackzone@gmail.com>
2024-11-27 13:17:31 +0630

    use render backend

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,1 +1,2 @@
-export const VITE_SERVER_URL = "https://wattschat-backend.vercel.app";
+export const VITE_SERVER_URL_VERCEL = "https://wattschat-backend.vercel.app";
+export const VITE_SERVER_URL = "https://chat-app-sclu.onrender.com";
